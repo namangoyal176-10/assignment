a62d46d3f54f973f03a57e3ab9a59a9dda5c1e20
naman goyal <namangoyal176@gmail.com>
2026-06-12 23:44:59 +0530

    Add files via upload

---
+++ stockspan.cpp
@@ -0,0 +1,33 @@
+//stock span problem
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    stack<int> s;
+
+    for (int i = 0; i < n; i++) {
+
+        while (!s.empty() && arr[i] >= arr[s.top()]) {
+            s.pop();
+        }
+
+        if (s.empty())
+            cout << i + 1 << " ";
+        else
+            cout << i - s.top() << " ";
+
+        s.push(i);
+    }
+
+    cout << endl;
+    return 0;
+}
